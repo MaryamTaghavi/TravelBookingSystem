98362cee1d9c3004d0a589549e8b7f41f4210007
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-10-28 13:05:13 +0330

    addIRepository

---
+++ TravelBookingSystem.Api/TravelBookingSystem.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace TravelBookingSystem.Domain.Interfaces;
+
+public interface IRepository<T> where T : class
+{
+    Task<T?> GetByIdAsync(int id);
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T> AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(int id);
+    Task<bool> ExistsAsync(int id);
+}
+
+public interface IFlightRepository : IRepository<Entities.Flight>
+{
+    Task<IEnumerable<Entities.Flight>> GetFlightsByFiltersAsync(string? origin, string? destination, DateTime? date);
+    Task<Entities.Flight?> GetByFlightNumberAsync(string flightNumber);
+    Task UpdateAvailableSeatsAsync(int flightId, int newSeatCount);
+}
