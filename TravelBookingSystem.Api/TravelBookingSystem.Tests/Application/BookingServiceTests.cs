053736525e9af6f690468589ae463dad97135282
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-10-29 10:47:17 +0330

    createBookingServiceTests

---
+++ TravelBookingSystem.Api/TravelBookingSystem.Tests/Application/BookingServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TravelBookingSystem.Domain.Interfaces;
+using TravelBookingSystem.Infrastructure.Repositories;
+using TravelBookingSystem.Infrastructure;
+
+namespace TravelBookingSystem.Tests.Application;
+
+public class BookingServiceTests : IDisposable
+{
+    private readonly AppDbContext _context;
+    private readonly IBookingRepository _bookingRepository;
+
+    public BookingServiceTests()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "MyTestDb")
+            .Options;
+
+        _context = new AppDbContext(options);
+        _bookingRepository = new BookingRepository(_context);
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+}
