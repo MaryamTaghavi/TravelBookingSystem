9a8a14728cd19e2815147946bb25f01b6ca7f9cb
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-10-30 11:33:14 +0330

    completeBaseEntity

--- src/TravelBookingSystem.Domain/Entities/BaseEntity.cs
+++ src/TravelBookingSystem.Domain/Entities/BaseEntity.cs
@@ -2,22 +2,12 @@
 
 namespace TravelBookingSystem.Domain.Entities;
 
-//TODO : Reach domain model
 public class BaseEntity
 {
-    /// <summary>
-    /// شناسه
-    /// </summary>
     [Key]
     public int Id { get; set; }
 
-    /// <summary>
-    /// تاریخ ویرایش
-    /// </summary>
     public DateTime? UpdateDate { get; set; }
 
-    /// <summary>
-    /// حذف شده؟
-    /// </summary>
     public bool IsDeleted { get; set; } = false;
 }
